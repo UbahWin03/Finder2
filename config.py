3b804e40bbbb67e5c203c186dd773b0227e91d6a
UbahWin03 <pomidorka03y@gmail.com>
2021-08-07 16:31:36 +0300

    + TG bot

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = ''
+token = '1923244547:AAFT3E1wY7ULAVdkrntOIkgj4_R0ULG4lbs'
--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
+from telebot import *
 from config import token
 
-snils = ''
-url = 'https://gumrf.ru/reserve/abitur/hod/?type=111'
+bot = telebot.TeleBot(token)
 
-m = []
-soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-
-tables = soup.find('table', {'class': 'table'})
-for i in range(6, 751):
-    table = tables.findAll('tr')[i].find_all('td')
-    if table[1].text == snils:
-        m = [table[i].text for i in range(len(table))]
-        print("место: ", m[0])
-        print("снилс: ", m[1])
-        print("общий балл: ", m[2])
-        print("есть ли согласие: ", m[-3])
-        print("согласие на другом направлении: ", m[-2])
-        #тут прога принтанет количество согласий, и моё место в списке согласий, а пока coming soon
+def makarovka(snils, mess):
+    m = []
+    soup = BeautifulSoup(requests.get('https://gumrf.ru/reserve/abitur/hod/?type=111').text, 'html.parser')
+    tables = soup.find('table', {'class': 'table'})
+    for i in range(6, 751):
+        table = tables.findAll('tr')[i].find_all('td')
+        if table[1].text == snils:
+            m = [table[i].text for i in range(len(table))]
+            bot.send_message(mess.chat.id, "место: " + str(m[0]))
+            bot.send_message(mess.chat.id, "общий балл: " + str(m[2]))
+            bot.send_message(mess.chat.id, "есть ли согласие: " + str(m[-3]))
+            bot.send_message(mess.chat.id, "согласие на другом направлении: " + str(m[-2]))
 
 
+@bot.message_handler(commands=['start'])
+def start(mess):
+    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #для будущих вузов
+    # knopka1 = types.KeyboardButton('1')
+    # knopka2 = types.KeyboardButton('2')
+    # markup.add(knopka1,knopka2)
+    bot.send_message(mess.chat.id, 'Привет, введи снилс')
+
+@bot.message_handler(content_types=['text'])
+def main(mess):
+    makarovka(str(mess.text), mess)
+
+bot.polling(none_stop=True)
 
 
