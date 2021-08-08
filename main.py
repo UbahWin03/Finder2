e420af996bfb28ef6e17f517cfccf7c5b4778955
UbahWin03 <pomidorka03y@gmail.com>
2021-08-08 22:00:50 +0300

    добавлено "моё место по согласиям"

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ from config import token
 bot = telebot.TeleBot(token)
 
 def makarovka(snils, mess):
-    m = []
     fl = True
+    count = 0
     soup = BeautifulSoup(requests.get('https://gumrf.ru/reserve/abitur/hod/?type=111').text, 'html.parser')
     tables = soup.find('table', {'class': 'table'})
+    napr = soup.find('summary', {'style': ' cursor: pointer;'})
+
     for i in range(6, 751):
         table = tables.findAll('tr')[i].find_all('td')
+        if table[8].text == 'Да' and fl:
+            count += 1
         if table[1].text == snils:
             fl = False
             m = [table[i].text for i in range(len(table))]
-            bot.send_message(mess.chat.id, "место: " + str(m[0]))
-            bot.send_message(mess.chat.id, "общий балл: " + str(m[2]))
-            bot.send_message(mess.chat.id, "есть ли согласие: " + str(m[-3]))
-            bot.send_message(mess.chat.id, "согласие на другом направлении: " + str(m[-2]))
+            bot.send_message(mess.chat.id, napr.text + '\n' + "место: " + str(m[0]) + '\n' + "общий балл: " + str(m[2]) + '\n' + "есть ли согласие: " + str(m[-3]) + '\n' + "согласие на другом направлении: " + str(m[-2]) + '\n' + 'мое место по согласиям: ' + str(count))
+
     if fl:
     	bot.send_message(mess.chat.id, 'нет такого снилса')
 
